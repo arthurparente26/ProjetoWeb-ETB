f33e7e5b834c3f54021b9a7d2cf1c27494b7d6d9
Arthur Parente~ <arthurparente26@gmail.com>
2024-02-15 10:41:05 -0300

    organização de pastas css e assets

--- lista.php
+++ lista.php
@@ -3,25 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Consulta de Usuarios</title>
-    <style>
-        body{
-            font-family: Arial, Helvetica, sans-serif;
-            background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
-            text-align: center;
-            color: white;
-        }
-        .box{
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%,-50%);
-            background-color: rgba(0, 0, 0, 0.6);
-            padding: 30px;
-            border-radius: 10px;
-        }
-        a:hover{
-            background-color: dodgerblue;
-        }
+    <link rel="stylesheet" href="/css/home.css">
     </style>
 </head>
 <body>
